1f9aa2f6e140c88e7c024b755d372132072ede20
Nithish@NetXD <154895474+NithishNetxd@users.noreply.github.com>
2025-06-03 13:29:12 +0530

    Update BeforeInstall.sh

--- scripts/BeforeInstall.sh
+++ scripts/BeforeInstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 
-cd /opt/code/
+cd /opt/test
 echo "Stopping existing service..."
-sudo systemctl stop codepipe || sudo service codepipe stop
+sudo systemctl stop test || sudo service codepipe stop
 
 echo "Backing up old binary..."
-sudo mv codepipeline BKP/codepipeline_$(date +%F)
+sudo mv gobuild BKP/gobuild$(date +%F)
 
 echo "Service stopped and backup created."
 exit 0
